64a980fdf05a648ea5fe45675fa8508d4cb49b8c
Bikki Mahato <42492682+bikkimahato@users.noreply.github.com>
2020-06-25 11:01:17 +0530

    Updates

---
+++ precache-manifest.8775b524b2bee584911e199d58e9d329.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f4de8d8683efef392a6a07b475535d88",
+    "url": "/React-Drag-and-Drop-Chessboard/index.html"
+  },
+  {
+    "revision": "51ff099680462a093457",
+    "url": "/React-Drag-and-Drop-Chessboard/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "4113f031abb405262287",
+    "url": "/React-Drag-and-Drop-Chessboard/static/js/2.f765865d.chunk.js"
+  },
+  {
+    "revision": "51ff099680462a093457",
+    "url": "/React-Drag-and-Drop-Chessboard/static/js/main.b8acfe43.chunk.js"
+  },
+  {
+    "revision": "44f34061b73ab28b2786",
+    "url": "/React-Drag-and-Drop-Chessboard/static/js/runtime-main.658cee3e.js"
+  }
+]);
